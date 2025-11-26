61ae99374f348cc30c7556af3d2589a91b886368
MY-C0DE <romewgolet@gmail.com>
2025-11-26 16:19:50 +0200

    creating and setting reouter

---
+++ app/Routers/router.class.php
@@ -0,0 +1,52 @@
+<?php
+    class Router {
+        private $Controller = "404";
+        private $method = "index";
+        // private $param = [];
+
+        private $routes= [
+        '/' =>'Home',
+        'login' =>'User',
+        'register' => 'User',
+    ];
+
+        public function __construct()
+        {
+            $url = $this->getUrl();
+            
+            $this->routToController($url);
+        }
+        public function getUrl(){
+            if(isset($_SERVER['REQUEST_URI'])){
+                $url = parse_url($_SERVER['REQUEST_URI'])['path'];
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                
+                $url = trim($url, '/');
+                // echo $url;
+                // $url = explode('/', $url);
+                return $url;
+            }
+        }
+        function routToController($url){
+
+            if($url != null){  
+                
+                $url = strtolower($url);   
+                                
+                if(array_key_exists($url, $this->routes)){
+                    
+                    $this->Controller = $this->routes[$url];
+                    
+                }        
+            }
+            require_once 'app/Controllers/'.$this->Controller.'.class.php';
+
+                if(method_exists($this->Controller, $url)){
+                    
+                    $this->method = $url;
+            }          
+             $class = $this->Controller;
+             $func = $this->method;
+             $class::$func();
+        }
+    }
